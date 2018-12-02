4ddb1e97b5efc9a19fbfbb066e0f6e0e4aa31fd5
Otis Goodman <hubbert.pumpkin@gmail.com>
2018-12-02 16:15:44 -0600

    Update

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const readline = require('readline');
 const config = require("./settings");
 const chalk = require("chalk")
-const DiscordWebhook = require("discord-webhooks");
+const Discord = require('discord.js');
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -9,30 +10,38 @@ const rl = readline.createInterface({
 
 
 
-function turmySay() {
-    rl.question('What Do You Want Turmy To Say ', (content) => {
-        turmy.execute({
-            content: `${content} Tbh`,
-            username: "Turmy",
-            avatar_url: config.avatarUrl
-        });
 
+function send(message, avatar,username,webhook) {
+                webhook.send(message, {
+                    "username": username,
+                    "avatarURL": avatar,
+
+                }).catch(error => {
+                        console.log(chalk.red(error));
+                    })
+
+
+}
+
+
+if (config.username == "" || config.avatarUrl =="" || config.url == "" || config.token == ""){
+    console.log(chalk.red('Error: Please Provide All Settings Fields'));
+
+}else{
+    const turmy = new Discord.WebhookClient(config.url, config.token);
+
+    rl.question('What Do You Want Turmy To Say ', (content) => {
+     send(content,config.avatarUrl,config.username,turmy)
 
     });
+
+
+
 }
 
 
 
 
-    if (config.url == "" || config.avatarUrl == ""){
-        let turmy = new DiscordWebhook(config.url);
-        turmy.on("ready", () => {
-            turmySay()
-        });
-    }else {
-        console.log(chalk.red('Error: Please Provide A Webhook Url And Or Img'));
-
-    }
 
 
 
