5468fa593ac8d396cef3d8902a2f6257d8a71979
Otis Goodman <hubbert.pumpkin@gmail.com>
2020-01-18 23:37:58 -0600

    Nightly Build (WIP)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-//Inits The Winows
 const {app, BrowserWindow} = require('electron');
 let mainWindow;
 let docsWindow;
-
-//Requires
-function createMain(){
+const path = require('path');
+const ipc = require('electron').ipcMain
+app.on('ready', () => {
   mainWindow = new BrowserWindow({
     width: 350,
     height: 500,
@@ -17,36 +16,50 @@ function createMain(){
     devTools: true
   }
 });
+mainWindow.setResizable(false);
+mainWindow.loadURL('file://' + __dirname + '/main.html');
+mainWindow.setIcon(path.join(__dirname, '/assets/icon.png'));
+});
+ipc.on('closeMain',function(event){
+    app.quit()
+     })
+ipc.on('swapDocs',function(event){
+
+docsWindow = new BrowserWindow({
+  width: 350,
+  height: 700,
+toolbar: null,
+frame: false,
+title: 'Turmy - Docs',
+transparent: true,
+webPreferences: {
+  nodeIntegration: true,
+  devTools: true
 }
-function createDocs(){
-  docsWindow = new BrowserWindow({
+});
+mainWindow.close();
+docsWindow.setResizable(false);
+docsWindow.loadURL('file://' + __dirname + '/docs.html');
+docsWindow.setIcon(path.join(__dirname, '/assets/icon.png'));
+})
+ipc.on('swapMain',function(event){
+  mainWindow = new BrowserWindow({
     width: 350,
-    height: 700,
+    height: 500,
   toolbar: null,
   frame: false,
-  title: 'Turmy - Docs',
+  title: 'Turmy',
   transparent: true,
   webPreferences: {
     nodeIntegration: true,
     devTools: true
   }
 });
-docsWindow.setResizable(false);
-docsWindow.loadURL('file://' + __dirname + '/docs.html');
-docsWindow.setIcon(path.join(__dirname, '/assets/icon.png'));
-}
-const path = require('path');
-const ipc = require('electron').ipcMain
-app.on('ready', () => {
-createMain()
-});
-ipc.on('closeMain',function(event){
-    app.quit()
-     })
-ipc.on('swapDocs',function(event){
-mainWindow.quit()
-createDocs()
-})  
+mainWindow.setResizable(false);
+mainWindow.loadURL('file://' + __dirname + '/main.html');
+mainWindow.setIcon(path.join(__dirname, '/assets/icon.png'));
+docsWindow.close();
+})
 app.on('window-all-closed', () => {
   app.quit();
 })
