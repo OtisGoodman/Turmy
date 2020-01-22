1e8f0d450318a5906fad46d5b6c2014e9fcfa57f
Otis Goodman <hubbert.pumpkin@gmail.com>
2020-01-22 09:53:26 -0600

    Working Git Links and Docs file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ let mainWindow;
 let docsWindow;
 const path = require('path');
 const ipc = require('electron').ipcMain
+const ipcRenderer = require('electron').ipcRenderer
+
+var resize = true;
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     width: 350,
@@ -20,46 +23,15 @@ mainWindow.setResizable(false);
 mainWindow.loadURL('file://' + __dirname + '/main.html');
 mainWindow.setIcon(path.join(__dirname, '/assets/icon.png'));
 });
-ipc.on('closeMain',function(event){
-    app.quit()
-     })
-ipc.on('swapDocs',function(event){
-
-docsWindow = new BrowserWindow({
-  width: 350,
-  height: 700,
-toolbar: null,
-frame: false,
-title: 'Turmy - Docs',
-transparent: true,
-webPreferences: {
-  nodeIntegration: true,
-  devTools: true
-}
-});
-mainWindow.close();
-docsWindow.setResizable(false);
-docsWindow.loadURL('file://' + __dirname + '/docs.html');
-docsWindow.setIcon(path.join(__dirname, '/assets/icon.png'));
-})
-ipc.on('swapMain',function(event){
-  mainWindow = new BrowserWindow({
-    width: 350,
-    height: 500,
-  toolbar: null,
-  frame: false,
-  title: 'Turmy',
-  transparent: true,
-  webPreferences: {
-    nodeIntegration: true,
-    devTools: true
-  }
-});
-mainWindow.setResizable(false);
-mainWindow.loadURL('file://' + __dirname + '/main.html');
-mainWindow.setIcon(path.join(__dirname, '/assets/icon.png'));
-docsWindow.close();
-})
 app.on('window-all-closed', () => {
   app.quit();
+})
+ipc.on('closeMain',function(event){
+    app.quit()
+})
+ipc.on('swapDocs',function(event){
+  mainWindow.loadURL('file://' + __dirname + '/docs.html');
+})
+ipc.on('swapMain',function(event){
+  mainWindow.loadURL('file://' + __dirname + '/main.html');
 })
