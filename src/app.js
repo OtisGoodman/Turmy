8453a1ff66ee4cf0120c9b1abf0933fb36321f60
Otis Goodman <hubbert.pumpkin@gmail.com>
2020-02-14 22:57:28 -0600

    Got Too Tired To Finish Maybe TMRW :P

--- src/app.js
+++ src/app.js
@@ -5,7 +5,53 @@ const path = require('path');
 const ipc = require('electron').ipcMain;
 const ipcRenderer = require('electron').ipcRenderer;
 const webhook = require("./webhookHandler.js");
+if (require('electron-squirrel-startup')) return;
 
+function handleSquirrelEvent(application) {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {
+        detached: true
+      });
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      spawnUpdate(['--createShortcut', exeName]);
+      setTimeout(application.quit, 1000);
+      return true;
+    case '--squirrel-uninstall':
+      spawnUpdate(['--removeShortcut', exeName]);
+      setTimeout(application.quit, 1000);
+      return true;
+    case '--squirrel-obsolete':
+      application.quit();
+      return true;
+  }
+};
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     width: 350,
